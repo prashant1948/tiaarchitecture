22f8557d399bcf6c4f42b90d46ebd63ea04159d8
prashant.thapa1948@gmail.com <prashant.thapa1948@gmail.com>
2021-05-15 19:53:04 +0545

    update

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/tia-architecture/vendor/autoload.php';
+require __DIR__.'/tiaarchitecture/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/tia-architecture/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/tia-architecture/bootstrap/app.php';
+$app = require_once __DIR__.'/tiaarchitecture/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
